354e5da751899b255e1660ff83042fdadb6e11ed
Boxu Fan <boxufan@gmail.com>
2021-09-24 15:27:40 -0500

    complete exercise

--- bash.js
+++ bash.js
@@ -1,13 +1,13 @@
 process.stdout.write('prompt >');
 
-// process.stdin.on('data', (data) => {
-//   // const cmd = data.toString().trim();
+process.stdin.on('data', (data) => {
+  // const cmd = data.toString().trim();
 
-//   // process.stdout.write('You typed: ' + cmd);
-//   // process.stdout.write('\nprompt >');
+  // process.stdout.write('You typed: ' + cmd);
+  // process.stdout.write('\nprompt >');
 
 
-// })
+})
 
 //const pwd = require("./pwd.js");
 //pwd();
